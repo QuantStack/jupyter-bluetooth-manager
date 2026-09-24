09ff49cbed483c9132789a2e75fc160265a42ae9
Florence Haudin <haudin.florence@gmail.com>
2026-09-24 13:23:11 +0200

    Fix missing export for everything from PairingInfoWidget.ts file.

--- packages/bluetooth/src/index.ts
+++ packages/bluetooth/src/index.ts
@@ -2,3 +2,4 @@ export * from './BluetoothManager';
 export * from './BluetoothDeviceRunningItem';
 export * from './DeviceOptions';
 export * from './DropDownRegistry';
+export * from './PairingInfoWidget';
